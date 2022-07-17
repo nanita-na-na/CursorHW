6a91a55ee8a65b704ba1c697ffe9631715b29695
Yanita <yanita2823@gmail.com>
2022-07-17 14:59:58 +0300

    Add corrections

--- src/main/java/HomeWork2.java
+++ src/main/java/HomeWork2.java
@@ -28,28 +28,31 @@ public class HomeWork2 {
                 array[i] = number + 1;
             }
         }
+        System.out.println("\nTask 1. Массив випадкових парних чисел: ");
         for (int i : array) {
             System.out.print(i + " ");
         }
     }
 
-/*
-    2. Вивести в консолі числа від 10 до 0
-*/
+    /*
+        2. Вивести в консолі числа від 10 до 0
+    */
     public static void numbersTenToNull() {
         int num = 11;
+        System.out.println("\nTask 2. Зворотній відлік: ");
         while (num > 0) {
             num--;
             System.out.print(num + " ");
         }
     }
 
-/*
-    3. Заповнити масив char випадковими символами
-*/
+    /*
+        3. Заповнити масив char випадковими символами
+    */
     public static void randomCharArray() {
         Random random = new Random();
         char[] array = new char[10];
+        System.out.println("\nTask 3. Массив випадкових символів: ");
         for (int i = 0; i < array.length; i++) {
             array[i] = (char) random.nextInt();
             System.out.print(array[i] + " ");
@@ -66,20 +69,20 @@ public class HomeWork2 {
         for (int i = 1; i < array.length; i++) {
             max = Math.max(max, array[i]);
         }
-        System.out.print(max + " ");
+        System.out.print("\nTask 4. Найбільше число в массиві: " + max);
     }
 
 
-/*
-    5. Знайти найменше число в масиві
-*/
+    /*
+        5. Знайти найменше число в масиві
+    */
     public static void minNumberInArray() {
         int[] array = {1, 2, 3, 4, 5, 17, 6, 7, 8, 9, 10, 11, 12};
         int min = array[0];
         for (int i = 1; i < array.length; i++) {
             min = Math.min(min, array[i]);
         }
-        System.out.print(min + " ");
+        System.out.print("\nTask 5. Найменше число в массиві: " + min);
     }
 
 /*
@@ -92,16 +95,17 @@ public class HomeWork2 {
         for (int i : array) {
             sum += i;
         }
-        System.out.print("Середнє арифметичне чисел: " + sum / array.length);
+        System.out.print("\nTask 6. Середнє арифметичне чисел: " + sum / array.length);
     }
 
-/*
-   7. Знайти найбільше парне число в масиві
-*/
+    /*
+       7. Знайти найбільше парне число в масиві
+    */
     public static void highestEvenNumberInArray() {
         int array[] = {5, 8, 12, 18, 54, 84, 91, 35, 17, 37};
         int arrayEven[] = new int[10];
         int max = array[0];
+        System.out.println("\nTask 7. Найбільше парне число в массиві: ");
 
 
         for (int i = 0; i < array.length; i++) {
@@ -121,6 +125,7 @@ public class HomeWork2 {
 
     public static void evenIndexArray() {
         int array[] = {5, 8, 12, 18, 54, 84, 91, 35, 17, 37};
+        System.out.println("\nTask 8. Значення парних індексів массива: ");
 
         for (int i = 0; i < array.length; i += 2) {
             System.out.print(array[i] + " ");

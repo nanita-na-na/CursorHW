a56ee025b5221f6536337c885ce0263d9c5f0286
Yanita <yanita2823@gmail.com>
2022-08-24 19:02:48 +0300

    Add HW 11

---
+++ src/main/java/homework11/RomantoArabic.java
@@ -0,0 +1,27 @@
+package homework11;
+
+import java.util.List;
+
+public class RomantoArabic implements AdapterNumber {
+    public void romanToArabic(String input) {
+        String romanNumeral = input.toUpperCase();
+        int result = 0;
+        List romanNumerals = RomanNumbers.getReverseSortedValues();
+
+        int i = 0;
+
+        while ((romanNumeral.length() > 0) && (i < romanNumerals.size())) {
+            RomanNumbers symbol = (RomanNumbers) romanNumerals.get(i);
+            if (romanNumeral.startsWith(symbol.name())) {
+                result += symbol.getValue();
+                romanNumeral = romanNumeral.substring(symbol.name().length());
+            } else {
+                i++;
+            }
+            if (romanNumeral.length() > 0) {
+                throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+            }
+        }
+        System.out.println(result);
+    }
+}
